82cdeb47f379f00c15c27a6d5c467dff8e470dcf
HueJo <slack_water@naver.com>
2024-08-23 09:33:12 +0900

    Create project

---
+++ SchoolMealBoard/src/main/java/com/zerobase/schoolmealboard/SchoolMealBoardApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.schoolmealboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMealBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMealBoardApplication.class, args);
+    }
+
+}
